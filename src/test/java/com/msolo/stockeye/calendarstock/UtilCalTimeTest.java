6a41487434b585ab687fd2089d2f150165a4b309
mSoloMoon <yue.zhi.msolo@gmail.com>
2014-11-17 09:58:40 +0800

    add UtilCalTimeTest.java unit test for TDD

---
+++ src/test/java/com/msolo/stockeye/calendarstock/UtilCalTimeTest.java
@@ -0,0 +1,46 @@
+package com.msolo.stockeye.calendarstock;
+
+import static org.junit.Assert.*;
+
+import java.util.GregorianCalendar;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UtilCalTimeTest {
+
+	private UtilCalTime utilCalTime;
+	
+	@Before
+		public void setUp() throws Exception {
+				utilCalTime = UtilCalTime.getInstance();
+			}
+
+	@After
+		public void tearDown() throws Exception {
+				utilCalTime = null;
+			}
+
+	@Test
+		public void testAssertClassSingleton() {
+			  UtilCalTime utilCalTimeAnother = UtilCalTime.getInstance();
+			  assertSame("should be same", utilCalTime, utilCalTimeAnother);
+			}
+
+	@Test
+		public void testGetIntCal() {
+			  int intCal = utilCalTime.getIntCal(new GregorianCalendar(2014, 10, 8));
+			  int intCalTest = 20141108;
+			  assertEquals("should be equal", intCal, intCalTest);
+			}
+	
+	@Test
+		public void testGetStrCal() {
+			  String strCal = utilCalTime.getStrCal(new GregorianCalendar(2014, 10, 8));
+			  String strCalTest = "20141108";
+			  assertEquals("should be equal", strCal, strCalTest);
+			}
+	
+}
+
