9f140f765597da52a931938bbc149a7b2d1cc51b
mSoloMoon <yue.zhi.msolo@gmail.com>
2014-11-17 10:23:10 +0800

    add javadoc

--- src/main/java/com/msolo/stockeye/calendarstock/UtilCalTime.java
+++ src/main/java/com/msolo/stockeye/calendarstock/UtilCalTime.java
@@ -8,16 +8,44 @@ public class UtilCalTime {
 	
 	private UtilCalTime() {}
 	
+	/**
+	 * 
+	 * This function is to return a UtilCalTime Object that apply the Singleton pattern.
+	 * 
+	 * @return UtilCalTime
+	 * 
+	 */
 	public static UtilCalTime getInstance() {
 			return INSTANCE;
 		}
 	
+	
+	/**
+	 * 
+	 * This function is to get the int representation of the GregorianCalendar object
+	 * 
+	 * Example : return 20141116
+	 * 
+	 * @param cal 
+	 * @return int
+	 * 
+	 */
 	public int getIntCal(GregorianCalendar cal) {		
-			return Integer.parseInt( getStrCal(cal) );
+			return Integer.parseInt( getStrCal(cal).replace("-", "") );
 		}
 
+	/**
+	 * 
+	 * This function is to get the String object of the GregorianCalendar object
+	 * 
+	 * Example : 2014-11-16
+	 * 
+	 * @param cal
+	 * @return String
+	 * 
+	 */
 	public String getStrCal(GregorianCalendar cal) {		
-			return String.format("%tF", cal).replace("-", "");
+			return String.format("%tF", cal);
 		}
 	
 }
